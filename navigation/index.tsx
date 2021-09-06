2a94d67bf34c315819cb5c18dd79f220c9572ec9
Kenneth Lien <kennethlien@berkeley.edu>
2021-09-05 18:10:23 -0700

    Initialize app from template

---
+++ navigation/index.tsx
@@ -0,0 +1,45 @@
+/**
+ * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
+ * https://reactnavigation.org/docs/getting-started
+ *
+ */
+import * as React from 'react';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ColorSchemeName } from 'react-native';
+
+import ModalScreen from '../screens/ModalScreen';
+import NotFoundScreen from '../screens/NotFoundScreen';
+import { RootStackParamList } from '../types';
+import LinkingConfiguration from './LinkingConfiguration';
+
+import TabNavigator from './TabNavigator';
+
+export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+  return (
+    <NavigationContainer
+      linking={LinkingConfiguration}
+      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
+
+/**
+ * A root stack navigator is often used for displaying modals on top of all other content.
+ * https://reactnavigation.org/docs/modal
+ */
+function RootNavigator() {
+  const Stack = createNativeStackNavigator<RootStackParamList>();
+  const { Navigator, Screen, Group } = Stack;
+
+  return (
+    <Navigator>
+      <Screen name="Root" component={TabNavigator} options={{ headerShown: false }} />
+      <Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+      <Group screenOptions={{ presentation: 'modal' }}>
+        <Screen name="Modal" component={ModalScreen} />
+      </Group>
+    </Navigator>
+  );
+}
