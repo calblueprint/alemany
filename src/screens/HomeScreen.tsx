88ebff7b75301ff95c83323b73a6e22ca7cbd010
Myles Domingo <mylesdomingo@berkeley.edu>
2021-09-30 23:13:49 -0700

    fixed linting issues

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,32 +5,32 @@ import EditScreenInfo from 'components/EditScreenInfo';
 import { Text, View } from 'components/Themed';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
 });
 
 export default function HomeScreen() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Home Screen</Text>
-            <View
-                style={styles.separator}
-                lightColor="#eee"
-                darkColor="rgba(255,255,255,0.1)"
-            />
-            <EditScreenInfo path="/screens/HomeScreen.tsx" />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <EditScreenInfo path="/screens/HomeScreen.tsx" />
+    </View>
+  );
 }
--- src/screens/TreeScreen.tsx
+++ src/screens/TreeScreen.tsx
@@ -5,32 +5,32 @@ import EditScreenInfo from 'components/EditScreenInfo';
 import { Text, View } from 'components/Themed';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
 });
 
 export default function TreeScreen() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Tab One</Text>
-            <View
-                style={styles.separator}
-                lightColor="#eee"
-                darkColor="rgba(255,255,255,0.1)"
-            />
-            <EditScreenInfo path="/screens/TreeScreen.tsx" />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab One</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <EditScreenInfo path="/screens/TreeScreen.tsx" />
+    </View>
+  );
 }
