3aa287425ca299038f4f9bebca5ec62dcae397e5
Nikki Suzani <nikkisuzani@Nikkis-MacBook-Pro.local>
2021-10-04 20:02:32 -0700

    restructure use effect hook to only query trees once

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -11,29 +11,40 @@ import ViewContainer from 'src/components/ViewContainer';
 
 export default function SearchScreen() {
   const [searchQuery, setSearchQuery] = useState('');
-  const [trees, setTrees] = React.useState<Tree[]>([]);
+  const [filteredTrees, setFilteredTrees] = useState<Tree[]>([]);
+  const [trees, setTrees] = useState<Tree[]>([]);
   useEffect(() => {
     async function getTrees() {
-      let data = await getAllTrees();
-      data = data.filter(
+      const data = await getAllTrees();
+      setTrees(data);
+      setFilteredTrees(data);
+    }
+    getTrees();
+  }, []);
+
+  const onSearchChange = (searchValue: string) => {
+    setSearchQuery(searchValue);
+    // Checking searchValue because searchQuery isn't set instantaneously
+    if (searchValue !== '') {
+      const newFilteredTrees = trees.filter(
         (tree: Tree) => (tree.name || '').toLowerCase().includes(searchQuery) ||
           (tree.id && tree.id.includes(searchQuery)),
       );
-      setTrees(data);
+      setFilteredTrees(newFilteredTrees);
+    } else {
+      setFilteredTrees(trees);
     }
-    getTrees();
-  }, [searchQuery]);
-  const onChangeSearch = (newQuery: string) => setSearchQuery(newQuery);
+  };
   return (
     <ViewContainer>
       <ScrollView>
         <Title> Search Screen </Title>
         <Searchbar
           placeholder="Search"
-          onChangeText={onChangeSearch}
+          onChangeText={onSearchChange}
           value={searchQuery}
         />
-        {trees.map((tree: Tree) => (
+        {filteredTrees.map((tree: Tree) => (
           <SearchCard key={tree.id} treeName={tree.name} treeID={tree.id} />
         ))}
         <EditScreenInfo path="/screens/TreeScreen.tsx" />
