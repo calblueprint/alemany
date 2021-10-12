d155d5d50660b1b225f24553efda9ccca390eb93
Annie Wang <wangannie@berkeley.edu>
2021-10-11 18:01:06 -0700

    Basic working deep linking

--- App.tsx
+++ App.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
-
+import * as Linking from 'expo-linking';
 import { StatusBar } from 'expo-status-bar';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
 import useCachedResources from 'hooks/useCachedResources';
 import useTheme from 'hooks/useTheme';
 import Navigation from 'navigation/index';
+import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const theme = useTheme();
+
+  const handleUrl = (event: { url: string }) => {
+    const { path, queryParams } = Linking.parse(event.url);
+    alert(
+      `Linked to app with path: ${path} and data: ${JSON.stringify(
+        queryParams,
+      )}`,
+    );
+  };
+
+  Linking.addEventListener('url', handleUrl);
 
   if (!isLoadingComplete) {
     return null;
