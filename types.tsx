c9318d986382e854927bf656267fbc9038052008
Kenneth <57934617+kennethlien@users.noreply.github.com>
2021-10-11 20:01:36 -0700

    Adding uuid fields in code to reflect Firestore changes (#24)

--- types.tsx
+++ types.tsx
@@ -44,6 +44,7 @@ export type Tree = {
   name: string | null;
   location: Location | null;
   planted: Planted | null;
+  uuid: string;
 };
 
 export type Location = {
@@ -57,12 +58,12 @@ export type Planted = {
 };
 
 export type Comment = {
-  id: string;
+  uuid: string;
   [key: string]: string;
 };
 
 export type Additional = {
-  id: string;
+  uuid: string;
   [key: string]: string;
 };
 
