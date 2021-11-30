292e431a8d3b401c127917218b773316bdb02037
Kenneth Lien <kennethlien@berkeley.edu>
2021-11-30 00:34:46 -0800

    Various input components

---
+++ src/components/Inputs.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+
+import { StyleSheet, TextInput } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import { Colors } from 'src/constants/Colors';
+
+const styles = StyleSheet.create({
+  short: {
+    borderWidth: 2,
+    borderColor: Colors.Grey100,
+    borderRadius: 8,
+    color: Colors.Grey700,
+    backgroundColor: Colors.White,
+    minWidth: '100%',
+    padding: 10,
+  },
+  title: {
+    fontSize: 17,
+    fontFamily: 'Inter_800ExtraBold',
+  },
+  multiline: {
+    minWidth: '100%',
+    minHeight: 100,
+    backgroundColor: Colors.White,
+  },
+  search: {
+    borderRadius: 8,
+    marginBottom: 10,
+    minWidth: '100%',
+    elevation: 0,
+  },
+  outline: {
+    borderWidth: 1,
+    borderColor: Colors.Grey100,
+  },
+});
+
+export function ShortInput(props: React.ComponentProps<typeof TextInput>) {
+  return (
+    <TextInput
+      {...props}
+      style={[styles.short, props.style]}
+      placeholderTextColor={Colors.Grey300}
+    />
+  );
+}
+
+export function TitleInput(props: React.ComponentProps<typeof TextInput>) {
+  return (
+    <TextInput
+      {...props}
+      style={[styles.title, props.style]}
+      placeholderTextColor={Colors.Grey300}
+    />
+  );
+}
+
+export function MultilineInput(props: React.ComponentProps<typeof TextInput>) {
+  return (
+    <TextInput
+      {...props}
+      style={[styles.multiline, props.style]}
+      multiline={true}
+      numberOfLines={4}
+    />
+  );
+}
+
+export function SearchBar(props: React.ComponentProps<typeof Searchbar>) {
+  return (
+    <Searchbar
+      {...props}
+      style={[styles.search, styles.outline, props.style]}
+    />
+  );
+}
