d82c2322aa3a4a6d58ad9c6bd38bf08806d47329
Kenneth Lien <kennethlien@berkeley.edu>
2021-11-29 21:41:33 -0800

    Basic containers and pill

---
+++ src/components/Components.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+
+import { StyleSheet, View, ScrollView, Text } from 'react-native';
+import { Colors } from 'src/constants/Colors';
+import { FontAwesome } from '@expo/vector-icons';
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: Colors.Background,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  scroll: {
+    backgroundColor: Colors.Grey50,
+  },
+  pill: {
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: Colors.Grey100,
+    padding: 10,
+    alignItems: 'center',
+  },
+});
+
+export function ScreenContainer(props: React.ComponentProps<typeof View>) {
+  return <View {...props} style={[styles.screen, props.style]} />;
+}
+
+export function ScrollContainer(
+  props: React.ComponentProps<typeof ScrollView>,
+) {
+  return <ScrollView {...props} style={[styles.scroll, props.style]} />;
+}
+
+export function RowContainer(props: React.ComponentProps<typeof View>) {
+  return <View {...props} style={[styles.row, props.style]} />;
+}
+
+type PillProps = {
+  value: string;
+  icon: string;
+};
+
+const defaultPillProps = {
+  value: '',
+  icon: '',
+};
+export function Pill(props: PillProps & React.ComponentProps<typeof View>) {
+  return (
+    <RowContainer style={[styles.pill, props.style]}>
+      {props.icon !== '' && (
+        <FontAwesome
+          name={props.icon}
+          color={Colors.Grey300}
+          style={{ marginRight: 4 }}
+        />
+      )}
+      <Text>{props.value}</Text>
+    </RowContainer>
+  );
+}
+
+Pill.defaultProps = defaultPillProps;
