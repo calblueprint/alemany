9645f4a1568cca8eda57ef23535e0afe4279ee58
Kenneth Lien <kennethlien@berkeley.edu>
2021-11-29 19:22:32 -0800

    text components

---
+++ src/components/Text.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import { StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  large: {
+    fontSize: 34,
+    fontFamily: 'Inter_800ExtraBold',
+  },
+  title: {
+    fontSize: 17,
+    fontFamily: 'Inter_700Bold',
+  },
+  caption: {
+    fontSize: 11,
+    fontFamily: 'Inter_400Regular',
+  },
+  body: {
+    fontSize: 15,
+    fontFamily: 'Inter_400Regular',
+  },
+  regular: {
+    fontFamily: 'Inter_400Regular',
+  },
+  medium: {
+    fontFamily: 'Inter_500Medium',
+  },
+  semibold: {
+    fontFamily: 'Inter_600SemiBold',
+  },
+  bold: {
+    fontFamily: 'Inter_700Bold',
+  },
+  extrabold: {
+    fontFamily: 'Inter_800ExtraBold',
+  },
+});
+
+export function LargeTitle(props: React.ComponentProps<typeof Text>) {
+  return <Text {...props} style={[styles.large, props.style]} />;
+}
+
+export function Title(props: React.ComponentProps<typeof Text>) {
+  return <Text {...props} style={[styles.title, props.style]} />;
+}
+
+export function Caption(props: React.ComponentProps<typeof Text>) {
+  return <Text {...props} style={[styles.caption, props.style]} />;
+}
+
+export function Body(props: React.ComponentProps<typeof Text>) {
+  return <Text {...props} style={[styles.body, props.style]} />;
+}
+
+export function Regular(props: React.ComponentProps<typeof Text>) {
+  return <Text {...props} style={[styles.regular, props.style]} />;
+}
+
+export function Medium(props: React.ComponentProps<typeof Text>) {
+  return <Text {...props} style={[styles.medium, props.style]} />;
+}
+
+export function SemiBold(props: React.ComponentProps<typeof Text>) {
+  return <Text {...props} style={[styles.semibold, props.style]} />;
+}
+
+export function Bold(props: React.ComponentProps<typeof Text>) {
+  return <Text {...props} style={[styles.bold, props.style]} />;
+}
+
+export function ExtraBold(props: React.ComponentProps<typeof Text>) {
+  return <Text {...props} style={[styles.extrabold, props.style]} />;
+}
