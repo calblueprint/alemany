fbe1b206b9e4d7a40ccffaf0727dc7fb5d45663f
Kabir Goel <kabirgoel.kg@gmail.com>
2022-03-10 16:12:14 -0800

    Replace react-native-paper Card with custom Card

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -1,18 +1,70 @@
 import React from 'react';
 
-import PropTypes from 'prop-types';
-import { Card } from 'react-native-paper';
+import { arrayOf, func, node, object, string } from 'prop-types';
+import { Pressable, StyleSheet, Text, View, ViewPropTypes } from 'react-native';
 
-export default function SearchCard({ name, id, onPress }) {
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#fff',
+    padding: 30,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  meta: {
+    marginTop: 10,
+    fontSize: 14,
+    color: '#333',
+  },
+});
+
+function Card({ onPress, children, style }) {
   return (
-    <Card style={{ height: 100, marginTop: 20 }} onPress={onPress}>
-      <Card.Title title={name} subtitle={id} />
+    <Pressable style={[styles.card, style]} onPress={onPress}>
+      {children}
+    </Pressable>
+  );
+}
+Card.propTypes = {
+  onPress: func,
+  children: node,
+  style: ViewPropTypes.style,
+};
+
+export default function SearchCard({ name, onPress, comments }) {
+  return (
+    <Card
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      }}
+      onPress={onPress}
+    >
+      <View>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.meta}>
+          {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+          {comments?.length || 0} comment
+          {comments?.length !== 1 && 's'}
+        </Text>
+      </View>
+      <View
+        style={{
+          height: 96,
+          width: 96,
+          backgroundColor: '#aaa',
+          borderRadius: 8,
+        }}
+      />
     </Card>
   );
 }
-
 SearchCard.propTypes = {
-  name: PropTypes.string,
-  id: PropTypes.string,
-  onPress: PropTypes.func,
+  name: string,
+  // eslint-disable-next-line react/forbid-prop-types
+  comments: arrayOf(object),
+  onPress: func,
 };
