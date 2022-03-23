045f3aa7d25b71432405fc0c0525ee22ad1d1d80
jenny (she/they) <jjiang17@berkeley.edu>
2022-03-23 13:35:48 -0700

    cleaning up code

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,11 +1,9 @@
-import React, { useRef } from 'react';
+import React from 'react';
 
 import { array, func, shape } from 'prop-types';
 import {
   StyleSheet,
   ViewPropTypes,
-  ScrollView,
-  Dimensions,
   SafeAreaView,
   FlatList,
   View,
@@ -13,7 +11,6 @@ import {
 import MapView, { Marker, Callout } from 'react-native-maps';
 
 import SearchCard from '../components/SearchCard';
-import ViewContainer from '../components/ViewContainer';
 import { DEFAULT_LOCATION } from '../constants/DefaultLocation';
 
 const styles = StyleSheet.create({
@@ -36,25 +33,11 @@ const styles = StyleSheet.create({
     shadowRadius: 4.65,
   },
 });
-// const scllView = React.useRef(null);
-// const onMarkerPress = mapEventData => {
-//   const markerID = mapEventData._targetInst.return.key;
 
-//   const x = markerID * CARD_WIDTH + markerID * 20;
-//   scllView.current.scrollTo({ x, y: 0, animated: true });
-// };
-
-// eslint-disable-next-line no-unused-vars
-export default function MapScreen({ style, navigation, data }) {
-  const CARD_WIDTH = Dimensions.get('window').width * 0.8;
+export default function MapScreen({ data }) {
   const renderItem = ({ item }) => (
     <SearchCard key={item.uuid} name={item.name} comments={item.comments} />
   );
-
-  const scrollToRef = ref => window.scrollTo(0, ref.current.offsetTop);
-  // General scroll to element function
-  const myRef = useRef(null);
-  const executeScroll = () => scrollToRef(myRef);
 
   return (
     <View>
@@ -65,7 +48,6 @@ export default function MapScreen({ style, navigation, data }) {
       >
         {data.map(tree => (
           <Marker
-            ref={myRef}
             key={tree.uuid}
             coordinate={{
               latitude: tree.location.latitude,
@@ -105,10 +87,6 @@ export default function MapScreen({ style, navigation, data }) {
   );
 }
 MapScreen.propTypes = {
-  style: ViewPropTypes.style,
   // eslint-disable-next-line react/forbid-prop-types
   data: array,
-  navigation: shape({
-    navigate: func,
-  }),
 };
