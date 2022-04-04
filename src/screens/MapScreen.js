874b109c756161759d2a2e8373c45569300f15a3
Nikki Suzani <nikkisuzani@berkeley.edu>
2022-04-03 18:57:16 -0700

    hide polygons

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -29,25 +29,24 @@ const styles = StyleSheet.create({
   },
 });
 
-const getCoordinates = mapboxJSON => {
-  const { features } = mapboxJSON;
-  return features.map(feature => {
-    const coordinates = feature.geometry.coordinates.map(coord => ({
-      latitude: coord[1],
-      longitude: coord[0],
-    }));
-    return (
-      <Polygon
-        id={mapboxJSON}
-        key={feature.name}
-        coordinates={coordinates}
-        fillColor="rgba(0,0,255,0.5)"
-        strokeColor="rgba(0,0,255,0.5)"
-        strokeWidth={2}
-      />
-    );
-  });
-};
+// const getCoordinates = mapboxJSON => {
+//   const { features } = mapboxJSON;
+//   return features.map(feature => {
+//     const coordinates = feature.geometry.coordinates.map(coord => ({
+//       latitude: coord[1],
+//       longitude: coord[0],
+//     }));
+//     return (
+//       <Polygon
+//         id={mapboxJSON}
+//         key={feature.name}
+//         coordinates={coordinates}
+//         strokeColor="rgba(0,0,255,0.5)"
+//         strokeWidth={2}
+//       />
+//     );
+//   });
+// };
 
 // eslint-disable-next-line no-unused-vars
 export default function MapScreen({ style, navigation, data }) {
@@ -59,7 +58,7 @@ export default function MapScreen({ style, navigation, data }) {
       maxZoomLevel={20}
       showsUserLocation
     >
-      {getCoordinates(MAPBOX_COORDS)}
+      {/* {getCoordinates(MAPBOX_COORDS)} */}
       {data.map(tree => (
         <Marker
           key={tree.uuid}
