eca6ba2fce75091a6b153ea8a3f42bd346a5c2f1
Andrew Wang <awang4@berkeley.edu>
2022-04-30 11:45:03 -0700

    changed default_location

--- src/constants/DefaultLocation.js
+++ src/constants/DefaultLocation.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 export const DEFAULT_LOCATION = {
-  latitude: 37.733053,
-  longitude: -122.419756,
+  latitude: 37.73335,
+  longitude: -122.4197,
   latitudeDelta: 0.0015,
   longitudeDelta: 0.00155,
 };
